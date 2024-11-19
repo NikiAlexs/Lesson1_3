d733b9fa8ae7298de648a9205c33d6ee1039bb13
NikiAlexs <nikilexs@gmail.com>
2024-11-19 17:17:19 +0700

    Add files via upload

---
+++ lesson 1_3 (19.11).py
@@ -0,0 +1,23 @@
+name = 'Urban'
+print(name, type(name))
+name = 10
+print(name, type(name))
+name = 45.5
+print(name, type(name))
+name = [1, 2]
+print(name, type(name))
+
+age = 30
+new_age = 31
+print(age+new_age)
+
+#Практическое задание "Динамическая типизация"
+name = 'NikiAlexs'
+print("Name:", name)
+age = 33
+print("Age:", age)
+age_new = 34
+age = age_new
+print("New Age:", age)
+is_student = True
+print("Is student:", is_student)
